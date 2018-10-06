15ff6bc4c4a0a475752f7012660bd732bfa87d56
Fisher Ankney <fisherankney@gmail.com>
2018-10-06 17:44:32 -0500

    exercises 1 - 6

---
+++ ex_2.py
@@ -0,0 +1,11 @@
+# comments
+
+# this is a comment 
+
+print('this is a statement')
+
+# it's useful that the comments are the same as R
+
+print('wow, Python is so great') # good code, right?
+
+# that's all for now. 
---
+++ ex_3.py
@@ -0,0 +1,30 @@
+# operators
+
+# print statement
+print('I will now count my chickens!')
+
+
+# We have 55 / 6 hens and some odd roosters
+print('Hens', 25 + 30 / 6) 
+print('Roosters', 100 - 25 * 3 % 4)
+
+print("now I will count the eggs:")
+
+# % means modulus, remainder of a%b a when divided by b
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# greater than / less than / equals to will compare values, return T/F
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("what is 3 + 2?", 3 + 2)
+print("what is 5 - 7?", 5 -7)
+
+print("Oh, that's why it's False") 
+
+print(" How about some more") 
+
+print("Is it greater?", 5 > -2)
+print('Is it greater or equal?', 5 >= -2)
+print('Is it less or equal?', 5 <= -2)
